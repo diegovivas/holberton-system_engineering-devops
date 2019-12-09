0e5ae2cf0559875f9e127ef1c659c965c796424b
diegovivas <dieguitovivas@hotmail.com>
2019-12-09 22:20:25 +0000

    fix indentation

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 #create file
 file {'/tmp/holberton':
-  mode   => '0744',
+  mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content   => 'I love Puppet'
+  content => 'I love Puppet'
 }
