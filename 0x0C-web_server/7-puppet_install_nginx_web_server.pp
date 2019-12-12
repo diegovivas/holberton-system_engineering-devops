c93ba364d0c500cd3d2f8838f3b6089c8d991430
diegovivas <dieguitovivas@hotmail.com>
2019-12-12 16:55:26 +0000

    sadsad

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,8 +2,7 @@
 exec { 'instal_ngnx':
   command  => 'sudo apt-get update && sudo apt-get -y install nginx &&
   echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html &&
-  var1="server_name _;\n\t rewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;"
-  && sudo sed -i "s/server_name _;/$var1/" /etc/nginx/sites-enabled/default && sudo service nginx start',
+  sudo service nginx start',
   provider => 'shell',
 }
 
