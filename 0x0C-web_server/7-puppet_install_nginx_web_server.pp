a7558995e08eb38a8b032c14f4d5797d73b233ab
diegovivas <dieguitovivas@hotmail.com>
2019-12-12 16:53:14 +0000

    puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,7 +1,9 @@
 #install ngnx
 exec { 'instal_ngnx':
   command  => 'sudo apt-get update && sudo apt-get -y install nginx &&
- echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html && sudo service nginx start',
+  echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html &&
+  var1="server_name _;\n\t rewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;"
+  && sudo sed -i "s/server_name _;/$var1/" /etc/nginx/sites-enabled/default && sudo service nginx start',
   provider => 'shell',
 }
 
