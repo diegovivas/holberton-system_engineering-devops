6e9ec311f322a429d57307221503e90e5c146246
diegovivas <dieguitovivas@hotmail.com>
2020-02-03 21:16:32 +0000

    [feature] task 0 and README

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,22 +5,24 @@ returns information about his/her TODO list progress."""
 import requests
 import sys
 
-text = "https://jsonplaceholder.typicode.com/users/" + sys.argv[1]
-user_req = requests.get(text)
-to_do = requests.get(text + "/todos")
-list_to_do = to_do.json()
-user = user_req.json()
-all_todo = 0
-done = 0
-list_done = []
-for element in list_to_do:
-    if element.get('userId') == int(sys.argv[1]):
-        all_todo += 1
-        if element.get('completed') is True:
-            done += 1
-            list_done.append(element.get('title'))
+if __name__ == "__main__":
+    text = "https://jsonplaceholder.typicode.com/users/" + sys.argv[1]
+    user_req = requests.get(text)
+    to_do = requests.get(text + "/todos")
+    list_to_do = to_do.json()
+    user = user_req.json()
+    all_todo = 0
+    done = 0
+    list_done = []
+    for element in list_to_do:
+        if element.get('userId') == int(sys.argv[1]):
+            all_todo += 1
+            if element.get('completed') is True:
+                done += 1
+                list_done.append(element.get('title'))
 
-print("Employee {} is done with tasks({}/{}):".format(user.get('name'),
-                                                      done, all_todo))
-for title in list_done:
-    print("\t {}".format(title))
+    print("Employee {} is done with tasks({}/{}):".format(user.get('name'),
+                                                          done, all_todo))
+    for title in list_done:
+        print("\t {}".format(title))
+
