1e85ea94df6e3ffad792cefb7074a4f187f7fa29
diegovivas <dieguitovivas@hotmail.com>
2020-02-06 21:51:10 +0000

    [fix]

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,15 +6,12 @@ import requests
 
 
 def top_ten(subreddit):
-    """ return the first 10 hot post
+    """ print the first 10 hot post
     for a selected subreddit """
     requ = requests.get(
         "https://api.reddit.com/r/{}/hot".format(subreddit),
         headers={"User-Agent": "cualquiera"},
         params={'limit': '10'})
 
-    if len(requ.json().get('data').get('children')):
-        for element in requ.json().get('data').get('children'):
-            print(element.get('data').get('title'))
-    else:
-        print('None')
+    for element in requ.json().get('data').get('children'):
+        print(element.get('data').get('title'))
