fdc42358f54c1f4c2f032c2214a3df9c2c904222
diegovivas <dieguitovivas@hotmail.com>
2020-02-27 17:42:56 +0000

    [feauture] task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 #web stack debugging 4
 exec { 'Correct file and restart':
-  command  => 'sudo echo 'ULIMIT="-n 15"'> /etc/default/nginx && sudo nginx restart',
+  command  => 'sudo echo 'ULIMIT="-n 2000"'> /etc/default/nginx && sudo nginx restart',
   provider => shell,
 }
