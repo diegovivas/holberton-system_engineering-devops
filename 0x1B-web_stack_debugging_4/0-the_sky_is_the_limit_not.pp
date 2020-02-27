588d5b1b42c91f60d80afedd549cbcb17a07000c
diegovivas <dieguitovivas@hotmail.com>
2020-02-27 18:06:53 +0000

    [fix] escape caracters

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 #web stack debugging 4
 exec { 'Correct file and restart':
-  command  => "sudo echo 'ULIMIT="-n 4096"'> /etc/default/nginx && sudo service nginx restart",
+  command  => "sudo echo 'ULIMIT=\"-n 4096\"' > /etc/default/nginx && sudo service nginx restart",
   provider => shell,
 }
